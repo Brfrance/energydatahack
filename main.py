e491a7929284808621f328b3a1d00cba33c707e6
Victor Li <victi91@gmail.com>
2021-03-31 15:03:57 +0200

    Model evaluation

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 """Main. Point d'entrer du programme."""
 
 import CNN
+import tensorflow as tf
 from CNN_data import get_data_sets
 
-TrainX, Trainy, ValidX, Validy, Test = get_data_sets(CNN.input_length)
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-cnn = CNN.compile_and_fit(TrainX, Trainy, ValidX, Validy)
-cnn.summary()
+TrainX, Trainy, ValidX, Validy, TestX, Testy = get_data_sets(CNN.input_length)
 
+cnn = CNN.compile_and_fit(TrainX, Trainy, ValidX, Validy, TestX, Testy)
+print(cnn)
 
 """
 TODO:
